c34e273639737c56ebcaf0d962a290690b361be5
TimPushkin <pushkin.td@gmail.com>
2022-03-24 15:46:08 +0300

    Add public methods with std types to DepthEstimator

--- lib/src/main/cpp/depth_estimator.cpp
+++ lib/src/main/cpp/depth_estimator.cpp
@@ -1,11 +1,34 @@
+#include <opencv2/calib3d.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
 #include "depth_estimator.h"
+#include "conversions.h"
 #include "logging.h"
 
 #define TAG "DepthEstimator"
 
+#define LOG_PARAMS \
+LOGD(TAG, "Loaded calibration parameters:\n" \
+"- LeftMapX has size %i x %i\n" \
+"- LeftMapY has size %i x %i\n" \
+"- RightMapX has size %i x %i\n" \
+"- RightMapY has size %i x %i\n" \
+"- Q has size %i x %i", \
+mLeftMap.first.cols, mLeftMap.first.rows, \
+mLeftMap.second.cols, mLeftMap.second.rows, \
+mRightMap.first.cols, mRightMap.first.rows, \
+mRightMap.second.cols, mRightMap.second.rows, \
+mQ.cols, mQ.rows)
+
 DepthEstimator::DepthEstimator(const cv::Mat &leftMapX, const cv::Mat &leftMapY, const cv::Mat &rightMapX,
                                const cv::Mat &rightMapY, const cv::Mat &Q) {
     loadCalibrationParams(leftMapX, leftMapY, rightMapX, rightMapY, Q);
+}
+
+DepthEstimator::DepthEstimator(const char *content, const std::string &leftMapXName,
+                               const std::string &leftMapYName, const std::string &rightMapXName,
+                               const std::string &rightMapYName, const std::string &qName) {
+    loadCalibrationParams(content, leftMapXName, leftMapYName, rightMapXName, rightMapYName, qName);
 }
 
 void DepthEstimator::loadCalibrationParams(const cv::Mat &leftMapX, const cv::Mat &leftMapY, const cv::Mat &rightMapX,
@@ -18,17 +41,29 @@ void DepthEstimator::loadCalibrationParams(const cv::Mat &leftMapX, const cv::Ma
     mRightMap.second = rightMapY;
     mQ = Q;
 
-    LOGD(TAG, "Loaded calibration parameters:\n"
-              "- LeftMapX has size %i x %i\n"
-              "- LeftMapY has size %i x %i\n"
-              "- RightMapX has size %i x %i\n"
-              "- RightMapY has size %i x %i\n"
-              "- Q has size %i x %i",
-         mLeftMap.first.cols, mLeftMap.first.rows,
-         mLeftMap.second.cols, mLeftMap.second.rows,
-         mRightMap.first.cols, mRightMap.first.rows,
-         mRightMap.second.cols, mRightMap.second.rows,
-         mQ.cols, mQ.rows);
+    LOG_PARAMS;
+}
+
+void DepthEstimator::loadCalibrationParams(const char *content, const std::string &leftMapXName,
+                                           const std::string &leftMapYName, const std::string &rightMapXName,
+                                           const std::string &rightMapYName, const std::string &qName) {
+    LOGI(TAG, "Loading calibration parameters from file contents...");
+
+    cv::FileStorage fs(content, cv::FileStorage::READ + cv::FileStorage::MEMORY);
+
+    if (!fs.isOpened()) {
+        LOGE(TAG, "Failed to read the received content");
+        // TODO: raise an exception
+        return;
+    }
+
+    fs[leftMapXName] >> mLeftMap.first;
+    fs[leftMapYName] >> mLeftMap.second;
+    fs[rightMapXName] >> mRightMap.first;
+    fs[rightMapYName] >> mRightMap.second;
+    fs[qName] >> mQ;
+
+    LOG_PARAMS;
 }
 
 void DepthEstimator::getDisparity(cv::InputArray leftImage, cv::InputArray rightImage,
@@ -66,8 +101,7 @@ void DepthEstimator::getDisparity(cv::InputArray leftImage, cv::InputArray right
     sgbm->compute(leftImageGray, rightImageGray, dst);
 }
 
-void DepthEstimator::getDepthFromDisparity(cv::InputArray disparity, cv::OutputArray dst,
-                                           float maxDepth) {
+void DepthEstimator::getDepthFromDisparity(cv::InputArray disparity, cv::OutputArray dst, float maxDepth) {
     LOGI(TAG, "Getting depth from disparity...");
 
     cv::Mat image3d, depthMap;
@@ -82,8 +116,8 @@ void DepthEstimator::getDepthFromDisparity(cv::InputArray disparity, cv::OutputA
     dst.assign(depthMap);
 }
 
-void DepthEstimator::calcDepth(cv::InputArray leftImage, cv::InputArray rightImage,
-                               cv::OutputArray dst, cv::Size imageSize) {
+void DepthEstimator::calcDepth(cv::InputArray leftImage, cv::InputArray rightImage, cv::OutputArray dst,
+                               cv::Size imageSize) {
     LOGI(TAG, "Calculating depth...");
 
     cv::Mat leftImage_, rightImage_, depthMap;
@@ -101,4 +135,23 @@ void DepthEstimator::calcDepth(cv::InputArray leftImage, cv::InputArray rightIma
     LOGI(TAG, "Depth calculation finished");
 
     dst.assign(depthMap);
+}
+
+std::vector<float> DepthEstimator::calcDepth(const std::vector<char> &leftImageEncoded,
+                                             const std::vector<char> &rightImageEncoded, int width, int height) {
+    LOGI(TAG, "Converting `char *` images into OpenCV classes...");
+
+    cv::Mat leftImage = cv::imdecode(leftImage, cv::IMREAD_COLOR),
+            rightImage = cv::imdecode(rightImage, cv::IMREAD_COLOR),
+            depthMap;
+
+    LOGD(TAG, "Loaded images:\n"
+              "- Left image has size %i x %i\n"
+              "- Right image has size %i x %i\n",
+         leftImageEncoded.cols, leftImageEncoded.rows,
+         rightImageEncoded.cols, rightImageEncoded.rows);
+
+    calcDepth(leftImageEncoded, rightImageEncoded, depthMap, cv::Size(width, height));
+
+    return matToVector<float>(depthMap);
 }
--- lib/src/main/cpp/depth_estimator.h
+++ lib/src/main/cpp/depth_estimator.h
@@ -1,28 +1,48 @@
 #ifndef SGBM_ANDROID_CORE_DEPTH_ESTIMATOR_H
 #define SGBM_ANDROID_CORE_DEPTH_ESTIMATOR_H
 
-#include <opencv2/calib3d.hpp>
-#include <opencv2/imgproc.hpp>
+#include <opencv2/core.hpp>
+
+#define LEFT_MAP_X_NAME "leftMapX"
+#define LEFT_MAP_Y_NAME "leftMapY"
+#define RIGHT_MAP_X_NAME "rightMapX"
+#define RIGHT_MAP_Y_NAME "rightMapY"
+#define Q_NAME "Q"
+
+#define IMG_WIDTH 640
+#define IMG_HEIGHT 360
 
 class DepthEstimator {
     std::pair<cv::Mat, cv::Mat> mLeftMap, mRightMap;
     cv::Mat mQ;
 
-    static void getDisparity(cv::InputArray leftImage, cv::InputArray rightImage,
-                             cv::OutputArray dst, int minDisparity, int maxDisparity,
-                             int blockSize = 1);
+    static void getDisparity(cv::InputArray leftImage, cv::InputArray rightImage, cv::OutputArray dst, int minDisparity,
+                             int maxDisparity, int blockSize = 1);
 
     void getDepthFromDisparity(cv::InputArray disparity, cv::OutputArray dst, float maxDepth = 10);
 
 public:
-    DepthEstimator(const cv::Mat &leftMapX, const cv::Mat &leftMapY, const cv::Mat &rightMapX,
-                   const cv::Mat &rightMapY, const cv::Mat &Q);
+    DepthEstimator(const cv::Mat &leftMapX, const cv::Mat &leftMapY, const cv::Mat &rightMapX, const cv::Mat &rightMapY,
+                   const cv::Mat &Q);
+
+    explicit DepthEstimator(const char *content, const std::string &leftMapXName = LEFT_MAP_X_NAME,
+                            const std::string &leftMapYName = LEFT_MAP_Y_NAME,
+                            const std::string &rightMapXName = RIGHT_MAP_X_NAME,
+                            const std::string &rightMapYName = RIGHT_MAP_Y_NAME, const std::string &qName = Q_NAME);
 
     void loadCalibrationParams(const cv::Mat &leftMapX, const cv::Mat &leftMapY, const cv::Mat &rightMapX,
                                const cv::Mat &rightMapY, const cv::Mat &Q);
 
+    void loadCalibrationParams(const char *content, const std::string &leftMapXName = LEFT_MAP_X_NAME,
+                               const std::string &leftMapYName = LEFT_MAP_Y_NAME,
+                               const std::string &rightMapXName = RIGHT_MAP_X_NAME,
+                               const std::string &rightMapYName = RIGHT_MAP_Y_NAME, const std::string &qName = Q_NAME);
+
     void calcDepth(cv::InputArray leftImage, cv::InputArray rightImage, cv::OutputArray dst,
-                   cv::Size imageSize = cv::Size(640, 360));
+                   cv::Size imageSize = cv::Size(IMG_WIDTH, IMG_HEIGHT));
+
+    std::vector<float> calcDepth(const std::vector<char> &leftImageEncoded, const std::vector<char> &rightImageEncoded,
+                                 int width = IMG_WIDTH, int height = IMG_HEIGHT);
 };
 
 #endif
