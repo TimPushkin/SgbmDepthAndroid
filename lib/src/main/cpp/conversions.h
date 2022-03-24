96d741fa92dd42e05cb4c4fb74f2cfec3fd72c6a
TimPushkin <pushkin.td@gmail.com>
2022-03-24 19:20:49 +0300

    Add core library usage example

--- lib/src/main/cpp/conversions.h
+++
@@ -1,9 +0,0 @@
-#ifndef SGBM_ANDROID_CORE_CONVERSIONS_H
-#define SGBM_ANDROID_CORE_CONVERSIONS_H
-
-#include <opencv2/core.hpp>
-
-template<class T>
-std::vector<T> matToVector(const cv::Mat &mat);
-
-#endif
