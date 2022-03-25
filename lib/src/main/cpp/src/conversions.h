2f4ee2d74f9edff03cfdcbe70da002409b62d0ac
TimPushkin <pushkin.td@gmail.com>
2022-03-25 20:07:30 +0300

    Fix examples building

--- lib/src/main/cpp/src/conversions.h
+++ lib/src/main/cpp/src/conversions.h
@@ -4,6 +4,11 @@
 #include "opencv2/core.hpp"
 
 template<class T>
-std::vector<T> matToVector(const cv::Mat &mat);
+std::vector<T> matToVector(const cv::Mat &mat) {
+    std::vector<T> result;
+    cv::Mat cont = mat.isContinuous() ? mat : mat.clone(); // make sure the Mat is continuous
+    cont.reshape(1, 1).copyTo(result); // copy from a 1D Mat
+    return result;
+}
 
 #endif
