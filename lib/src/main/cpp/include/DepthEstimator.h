682c51313286e48b49ef16a2110766b758543c28
TimPushkin <pushkin.td@gmail.com>
2022-04-10 17:54:06 +0300

    Remove some constants from DepthEstimator

--- lib/src/main/cpp/include/DepthEstimator.h
+++ lib/src/main/cpp/include/DepthEstimator.h
@@ -5,24 +5,36 @@
 
 class DepthEstimator {
 public:
-    std::string leftMapXName = "leftMapX", leftMapYName = "leftMapY", rightMapXName = "rightMapX",
-            rightMapYName = "rightMapY", qName = "Q";
-
     explicit DepthEstimator(const char *content);
 
-    void loadCalibrationParams(const char *content);
+    void loadCalibrationParams(const char *content, const std::string &leftMapXName = "leftMapX",
+                               const std::string &leftMapYName = "leftMapY",
+                               const std::string &rightMapXName = "rightMapX",
+                               const std::string &rightMapYName = "rightMapY", const std::string &qName = "Q");
 
     std::vector<float> calcDepth(const std::vector<char> &leftImage, const std::vector<char> &rightImage,
-                                 int width = 640, int height = 360);
+                                 int width = 640, int height = 360) const;
+
+    void setMinDisparity(int value);
+
+    void setMaxDisparity(int value);
+
+    void setBlockSize(int value);
+
+    void setDisparityCorrectionFactor(float value);
+
+    void setMaxDepth(float value);
 
 private:
     std::pair<cv::Mat, cv::Mat> mLeftMap, mRightMap;
     cv::Mat mQ;
 
-    static void getDisparity(cv::InputArray leftImage, cv::InputArray rightImage, cv::OutputArray dst, int minDisparity,
-                             int maxDisparity, int blockSize = 1);
+    int minDisparity = 0, maxDisparity = 112, blockSize = 3;
+    float disparityCorrectionFactor = (1.0f / 16), maxDepth = 10;
 
-    void getDepthFromDisparity(cv::InputArray disparity, cv::OutputArray dst, float maxDepth = 10);
+    void getDisparity(cv::InputArray leftImage, cv::InputArray rightImage, cv::OutputArray dst) const;
+
+    void getDepthFromDisparity(cv::InputArray disparity, cv::OutputArray dst) const;
 };
 
 #endif
